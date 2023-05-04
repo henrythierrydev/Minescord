e1b8b1f0c586620c4cbbdcdb41aa3249ec601ca8
HenryDM <henrythierrydev@gmail.com>
2023-05-03 23:32:39 -0300

    Add bot status

---
+++ Events/Status.js
@@ -0,0 +1,27 @@
+const { ActivityType } = require('discord.js');
+const config = require('../Resources/status.json');
+  
+async function managePresence(client) 
+{
+    const { presences, status } = config;
+    let currentActivityIndex = 0;
+
+    client.user.setStatus(status);
+    
+    function setNextActivity() {
+        const activity = presences[currentActivityIndex];
+        const { description, type, interval } = activity;
+
+        client.user.setActivity(description, {
+            type: ActivityType[type]
+        });    
+        currentActivityIndex = (currentActivityIndex + 1) % presences.length;
+
+        setTimeout(() => {
+            setNextActivity();
+        }, interval);
+    }
+    setNextActivity();
+}
+
+module.exports = managePresence;
