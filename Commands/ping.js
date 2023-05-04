138c4f3ae6f680e5ef55e8f8dba49694d4ef1ab4
HenryDM <henrythierrydev@gmail.com>
2023-05-03 23:05:17 -0300

    Better command

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,20 +1,26 @@
 const { EmbedBuilder } = require('discord.js');
 const CommandData = require('../resources/commands.json');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
+ 
 module.exports = {
     data: new SlashCommandBuilder()
        .setName(CommandData.ping.name)
        .setDescription(CommandData.ping.description),
  
     async execute(interaction) {
-       const embed = new EmbedBuilder()
-          .setTitle(CommandData.ping.embed.title)
-          .setDescription(CommandData.ping.embed.description.replace('{user_ping}', `${Date.now() - interaction.createdTimestamp}`))
-          .setColor(CommandData.ping.embed.color);
- 
-       await interaction.reply({
-          embeds: [embed]
-       });
+        const ping = interaction.client.ws.ping;
+        const botPing = interaction.client.ws.ping;
+        const userMention = interaction.user.toString();
+
+        const embed = new EmbedBuilder()
+           .setTitle(CommandData.ping.embed.title)
+           .setColor(CommandData.ping.embed.color)
+           .setDescription(CommandData.ping.embed.description.replace('{user_ping}', ping).replace('{bot_ping}', botPing));
+        
+           await interaction.reply({
+            content: `${userMention}`,
+            embeds: [embed],
+            ephemeral: false
+        });
     },
 };
