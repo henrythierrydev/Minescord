4c999f6b48de09dff005a902aec68639013985df
HenryDM <henrythierrydev@gmail.com>
2023-05-06 20:40:30 -0300

    Start socials module

---
+++ Commands/General/socials.js
@@ -0,0 +1,42 @@
+const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder} = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const { getMessages, getCommands } = require('../../Languages/controller');
+const { execute } = require('../Config/language');
+const messages = getMessages();
+const commands = getCommands();
+
+// -------------------
+//    COMMAND START
+// -------------------
+
+module.exports = {
+
+    // -------------------
+    //   COMMAND BUILDER
+    // -------------------
+
+    data: new SlashCommandBuilder()
+        .setName(commands.socials.name)
+        .setDescription(commands.socials.description),
+
+    // -------------------
+    //   COMMAND EXECUTE
+    // -------------------
+
+    async execute(interaction)
+    {
+        const userMention = interaction.user.toString();
+        const authorAvatar = interaction.member.user.avatarURL();
+        
+        // -------------------
+        //    EMBED BUILDER
+        // -------------------
+        
+        const socialembed = new EmbedBuilder()
+            .setTimestamp()
+            .setTitle(commands.socials.embed.title)
+            .setDescription(commands.socials.embed.description)
+            .setColor("")
+    }
+}
