138f04586d4aeb7b9aff700ec6901f76f31446e0
HenryDM <henrythierrydev@gmail.com>
2023-05-07 16:45:42 -0300

    new translation method

---
+++ Languages/controller.js
@@ -0,0 +1,41 @@
+const config = require("../config.json");
+let lang = config.lang || "en";
+
+// -------------------
+//   SUPPORTED LANGS
+// -------------------
+
+const languages = ["en", "es", "pt"];
+
+// -------------------
+//   SUPPORTED LANGS
+// -------------------
+
+if(!languages.includes(lang)) 
+{
+    console.log("[Minescord] => [C] Critical => Selected Language not found!");
+    console.log("[Minescord] => [L] Log => Default lang seted to English!");
+    lang = "en";
+}
+
+// -------------------
+//   GET TRANSLATION
+// -------------------
+
+const commandLang = languages.reduce((acc, language) => {
+    acc[language] = {
+        commands: require(`./translations/${language}`)
+    };
+
+    return acc;
+}, {});
+
+// -------------------
+//   RETURN THE LANG
+// -------------------
+
+function getTranslation() {
+    return commandLang[lang].commands;
+}
+
+module.exports = { getTranslation };
