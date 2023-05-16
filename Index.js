211e9c303d95fcbcb9e0245cebcf697062f19334
HenryDM <henrythierrydev@gmail.com>
2023-05-16 00:21:23 -0300

    prepare minescord next gen

--- Index.js
+++ Index.js
@@ -1,41 +1,40 @@
 const fs = require('fs');
 const managePresence = require('./events/status');
-const { token, guild_id } = require('./config.json');
-const { Client, Collection} = require('discord.js');
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const client = new Client({ intents: [1, 512, 32768, 2, 128] });
+const { token } = require('./config.json');
 
-// ================
-//     COMMANDS
-// ================
-// Function to verify and create new bot commands folder.
+const { Client, Collection } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const client = new Client({ intents: [1, 512, 32768, 2, 128]});
+
+// -------------------
+//    CMD REGISTER
+// -------------------
 
 client.commands = new Collection();
 const commandFolders = fs.readdirSync('./commands');
 
 for(const folder of commandFolders) 
 {
-    const folderFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));    
-    
-    for(const file of folderFiles) {
+    const folderFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+
+    for(const file of folderFiles) 
+    {
         const command = require(`./commands/${folder}/${file}`);
 
-        if(command.data instanceof SlashCommandBuilder) {
+        if(command.data instanceof SlashCommandBuilder) 
+        {
             client.commands.set(command.data.name, command);
         }
     }
 }
 
-// ================
-//    BOT READY
-// ================
-// Function that executes every that the bot starts.
+// -------------------
+//      BOT LOAD
+// -------------------
 
-client.on('ready', async () => 
-{
-    const guildId = guild_id;
+client.on('ready', async () => {
     const commandData = client.commands.map(command => command.data.toJSON());
-    await client.application.commands.set(commandData, guildId);
+    await client.application.commands.set(commandData);
 
     console.log(`[Minescord] => [v] Sucess => Logged in as ${client.user.tag}!`);
     console.log(`[Minescord] => [!] Alert => ${commandData.length} registered commands.`);
@@ -44,16 +43,20 @@ client.on('ready', async () =>
     managePresence(client);
 });
 
-// =================
-//   SLASH COMMAND
-// =================
-// Funtion to create and verify the bot slash commands
+// -------------------
+//    BOT COMMANDS
+// -------------------
 
 client.on('interactionCreate', async interaction => 
 {
     if(!interaction.isCommand()) return;
+
+    if(!interaction.guild) {
+        return interaction.reply('Este bot não suporta comandos em mensagem direta.');
+    }
+
     const command = client.commands.get(interaction.commandName);
-    
+
     if(!command) return;
     await command.execute(interaction).catch(console.error);
 });
