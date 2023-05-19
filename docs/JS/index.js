91aaa24d452f0eb5616deb3b7079f748bf595f61
HenryDM <henrythierrydev@gmail.com>
2023-05-19 16:44:24 -0300

    Ready!

--- docs/JS/index.js
+++ docs/JS/index.js
@@ -1,5 +1,15 @@
 const github = document.querySelector('#github');
+const minescordgithub = document.querySelector('#minescord-github');
+const minescordwiki = document.querySelector('#minescord-wiki');
 
 github.addEventListener("click", function() {
     window.open('https://github.com/Henry8K', '_blank');
+});
+
+minescordgithub.addEventListener("click", function() {
+    window.open('https://github.com/Henry8K/Minescord/', '_blank');
+});
+
+minescordwiki.addEventListener("click", function() {
+    window.open('https://henry8k.gitbook.io/minescord-wiki/', '_blank');
 });
